fd6e4f7db0b220387f69e1c6c7217d495e1de412
nishant129 <nishantborkar139@gmail.com>
2023-07-07 23:28:09 +0530

    model deploy

--- scr/components/data_ingestion.py
+++ scr/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from scr.components.data_transformation import DataTransformation
 from scr.components.data_transformation import DataTransformationConfig
 
+from scr.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifact","train.csv")
@@ -54,4 +56,8 @@ if __name__ == '__main__':
     
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
     
