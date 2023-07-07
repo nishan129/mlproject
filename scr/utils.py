a8b95b62055b6d2652df4c93dfd107fe27a37a8f
nishant129 <nishantborkar139@gmail.com>
2023-07-08 01:01:30 +0530

    pipeline

--- scr/utils.py
+++ scr/utils.py
@@ -6,35 +6,34 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 from scr.exception import CustomException
 
-def save_objects(file_path, obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        
-        os.makedirs(dir_path,exist_ok=True)
-        
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
-            
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
-    
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            #gs = GridSearchCV(model,para,cv=3)
-            #gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            #model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -50,6 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
