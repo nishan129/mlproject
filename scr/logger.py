34209808ebb22571fb1271f6ece7ab3749855b36
nishant129 <nishantborkar139@gmail.com>
2023-07-07 19:33:22 +0530

    Data Ingestion

--- scr/logger.py
+++ scr/logger.py
@@ -2,8 +2,8 @@ import logging
 import os 
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%s')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
